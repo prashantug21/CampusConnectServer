c5d1f38cf5999c2dfe8aa86784993161e84cd51e
prashantug21 <99468316+prashantug21@users.noreply.github.com>
2023-04-27 18:26:53 +0530

    Update Login.js

--- Routes/Login.js
+++ Routes/Login.js
@@ -6,7 +6,8 @@ const User=require('../model/userModel');
 const otpRequests=require('../model/otpRequests');
 
 async function login(req,res){
-    try{
+//     try{
+    console.log(1)
         const {email,otp}=req.body;
         const user=await User.findOne({email:email});
         const sentOTP = await otpRequests.findOne({email:email});
@@ -23,17 +24,19 @@ async function login(req,res){
         if(sentOTP.OTP!=otp){
             return res.status(400).json({message:'Invalid OTP'});
         }
+    console.log(2)
         const token=jwt.sign({_id:user._id},secretKey,{expiresIn:'30d'});
         res.cookie('jwtToken', token, {
             path:'/',
+            httpOnly:true;
             samesite:'none',
             secure:true,
             maxAge: 30*24*3600000
           }).status(201).json({message:"Login Successful","username":user.username});
           await otpRequests.deleteOne({email:email})
-    }catch(e){
-        return res.status(400).json({message:'Some Error has occured'});
-    }
+//     }catch(e){
+//         return res.status(400).json({message:'Some Error has occured'});
+//     }
     
 }
 
